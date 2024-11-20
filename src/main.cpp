28e1a2b6b146ce6783a8950255b62e9b5819b56a
whoami <13003827890@163.com>
2024-11-21 00:28:55 +0800

    add some init file.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author whoami (13003827890@163.com)
+ * @brief main source file
+ * @version 0.1
+ * @date 2024-11-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
